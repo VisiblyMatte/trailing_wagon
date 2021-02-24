45d9d7c36791330b592517de35aeca2c635ad838
VisiblyMatte <45577808+VisiblyMatte@users.noreply.github.com>
2021-02-24 09:23:29 -0800

    Added file description header

--- movement.py
+++ movement.py
@@ -1,1 +1,10 @@
-
+# Wagon Movement API - Software interface with hardware
+#
+# Once we've figured out where the wagon needs to be, this
+# will be the interface we use to get the wagon moving in
+# in the right direction.
+#
+# No logistic calculations should go here, this is for
+# interfacing with hardware after the calculation has
+# been made.
+#
